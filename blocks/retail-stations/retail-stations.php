3dda1c60cd5c99e833d04b1882efac6567db1440
Harpalsinh <harpalsinh@7span.com>
2025-04-03 19:45:47 +0530

    Changes of url on retail stations.

--- blocks/retail-stations/retail-stations.php
+++ blocks/retail-stations/retail-stations.php
@@ -72,7 +72,7 @@ $className = $block['className'] ?? '';
                             $station_name = get_sub_field('station_name');
                             $station_address = get_sub_field('station_address');
                             $direction_link = get_sub_field('direction_link');
-
+                            // echo '<pre>';print_r($direction_link);
                             // Station Timing ACF Field
                             $station_weekdays_starting_time = get_sub_field('station_weekdays_starting_time');
                             $station_weekdays_closing_time = get_sub_field('station_weekdays_closing_time');
@@ -241,15 +241,20 @@ $className = $block['className'] ?? '';
                                                         </div>
                                                     <?php endif;?>
 
-                                                    <div class="direction-btn">
-                                                        <a href="service.html#retailNetwork" class="join-us-btn red-btn">
-                                                            <div class="join-us">Get Directions</div>
-                                                            <svg class="svg-sub" xmlns="http://www.w3.org/2000/svg" width="21" height="12" viewBox="0 0 21 12" fill="none">
-                                                                <path d="M20.5303 6.53033C20.8232 6.23744 20.8232 5.76256 20.5303 5.46967L15.7574 0.696699C15.4645 0.403806 14.9896 0.403806 14.6967 0.696699C14.4038 0.989593 14.4038 1.46447 14.6967 1.75736L18.9393 6L14.6967 10.2426C14.4038 10.5355 14.4038 11.0104 14.6967 11.3033C14.9896 11.5962 15.4645 11.5962 15.7574 11.3033L20.5303 6.53033ZM0 6.75H20V5.25H0V6.75Z" fill="white" />
-                                                            </svg>
+                                                    <?php 
+                                                        if($direction_link['title'] != ''){ ?>
+                                                            <div class="direction-btn">
+                                                                <a href="<?php echo $direction_link['url']; ?>" class="join-us-btn red-btn">
+                                                                    <div class="join-us"><?php echo $direction_link['title']; ?></div>
+                                                                    <svg class="svg-sub" xmlns="http://www.w3.org/2000/svg" width="21" height="12" viewBox="0 0 21 12" fill="none">
+                                                                        <path d="M20.5303 6.53033C20.8232 6.23744 20.8232 5.76256 20.5303 5.46967L15.7574 0.696699C15.4645 0.403806 14.9896 0.403806 14.6967 0.696699C14.4038 0.989593 14.4038 1.46447 14.6967 1.75736L18.9393 6L14.6967 10.2426C14.4038 10.5355 14.4038 11.0104 14.6967 11.3033C14.9896 11.5962 15.4645 11.5962 15.7574 11.3033L20.5303 6.53033ZM0 6.75H20V5.25H0V6.75Z" fill="white" />
+                                                                    </svg>
 
-                                                        </a>
-                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <?php  
+                                                        }
+                                                    ?>
                                                 </div>
                                             </div>
                                         </div>
