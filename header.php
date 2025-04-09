3a7b53f098cd10ba949ae01e9054a7827d58bede
munish-7span <munish@7span.com>
2025-04-09 19:21:36 +0530

    Modify Page header and its css

--- header.php
+++ header.php
@@ -25,10 +25,10 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<?php if (is_active_sidebar('header_widget')) : ?>
-		<div class="header-widget-area">
+		<!-- <div class="header-widget-area"> -->
 			<?php dynamic_sidebar('header-widget'); ?>
 			<div class="submenu-overlay"></div>
-		</div>
+		<!-- </div> -->
 	<?php endif; ?>
 	<div id="page" class="site">
 	
